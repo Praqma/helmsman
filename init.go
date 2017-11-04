1d4e96ff2f30edd3eada3b7a85c1adedd6358f53
Sami Alajrami <sami@praqma.com>
2017-11-04 13:59:41 +0100

    adding a check to validate kubectl is installed.

--- init.go
+++ init.go
@@ -8,21 +8,27 @@ import (
 // init is executed before the main is executed.
 // It checks if Helm exists and configures the connection to the k8s cluster.
 func init() {
-	if !helmExists() {
+	if !toolExists("helm") {
 		log.Fatal("ERROR: helm is not installed/configured correctly. Aborting!")
 		os.Exit(1)
 	}
 
+	if !toolExists("kubectl") {
+		log.Fatal("ERROR: kubectl is not installed/configured correctly. Aborting!")
+		os.Exit(1)
+	}
+
 	// TODO : setup cluster connection
+
 }
 
-// helmExists return true if Helm is present in the environment and false otherwise.
-// It uses the Helm command to check if it is recognizable or not.
-func helmExists() bool {
+// toolExists returns true if the tool is present in the environment and false otherwise.
+// It takes as input the tool's command to check if it is recognizable or not. e.g. helm or kubectl
+func toolExists(tool string) bool {
 	cmd := command{
 		Cmd:         "bash",
-		Args:        []string{"-c", "helm "},
-		Description: "validating that helm is installed.",
+		Args:        []string{"-c", tool},
+		Description: "validating that " + tool + " is installed.",
 	}
 
 	exitCode, _ := cmd.exec(debug)
