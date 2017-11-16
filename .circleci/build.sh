74dca4b650edb28bf76e3a44237b893f8855f12b
Sami Alajrami <sami@praqma.com>
2017-11-16 18:00:26 +0100

    #10 updated the circleci script

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,19 +3,23 @@ echo "running tests ..."
 go test -v
 
 if [ $? -eq 0 ]; then
-    echo "releasing ..."
-    goreleaser | grep -o "error"
+    echo "building ..."
+    go build
+
     if [ $? -eq 0 ]; then
-        goreleaser | grep -o "already_exists"
-        if [ $? -gt 0 ]; then
-            echo "ERROR: failed to release. Terminating."
-            exit 9
+        echo "cleaning after tests ..."
+        rm hello.world hello.world1 helmsman
+        echo "releasing ..."
+        goreleaser | tee /dev/tty | grep -o "error"
+        if [ $? -eq 0 ]; then
+            echo "goreleaser experienced an error and no new releases made. That is Ok!"
         else
-            echo "No new releases made. That is Ok!"
-        fi    
+            echo "New release was successfully made."    
+        fi
     else
-        echo "New release was successfully made."    
-    fi
+        echo "Build failed!!"
+        exit 9   
+    fi 
 else
     echo "tests failed ... Aborting!"
     exit 9
