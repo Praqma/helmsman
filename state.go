31a808e92ad608c234b6807fc3bba2775c2bb2ed
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2018-05-29 10:47:58 -0400

    Thanks to Dave for the fixups

--- state.go
+++ state.go
@@ -1,208 +1,204 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/url"
-	"os"
-	"strings"
+  "fmt"
+  "log"
+  "net/url"
+  "os"
+  "strings"
 )
 
 // namespace type represents the fields of a namespace
 type namespace struct {
-	Protected            bool
-	InstallTiller        bool
-	TillerServiceAccount string
-	CaCert               string
-	TillerCert           string
-	TillerKey            string
-	ClientCert           string
-	ClientKey            string
+  Protected            bool
+  InstallTiller        bool
+  TillerServiceAccount string
+  CaCert               string
+  TillerCert           string
+  TillerKey            string
+  ClientCert           string
+  ClientKey            string
 }
 
 // state type represents the desired state of applications on a k8s cluster.
 type state struct {
-	Metadata     map[string]string
-	Certificates map[string]string
-	Settings     map[string]string
-	Namespaces   map[string]namespace
-	HelmRepos    map[string]string
-	Apps         map[string]*release
+  Metadata     map[string]string
+  Certificates map[string]string
+  Settings     map[string]string
+  Namespaces   map[string]namespace
+  HelmRepos    map[string]string
+  Apps         map[string]*release
 }
 
 // validate validates that the values specified in the desired state are valid according to the desired state spec.
 // check https://github.com/Praqma/Helmsman/docs/desired_state_spec.md for the detailed specification
 func (s state) validate() (bool, string) {
 
-	// settings
-	if s.Settings == nil || len(s.Settings) == 0 {
-		return false, "ERROR: settings validation failed -- no settings table provided in TOML."
-	} else if value, ok := s.Settings["kubeContext"]; !ok || value == "" {
-		return false, "ERROR: settings validation failed -- you have not provided a " +
-			"kubeContext to use. Can't work without it. Sorry!"
-	} else if value, ok = s.Settings["clusterURI"]; ok {
+  // settings
+  if s.Settings == nil || len(s.Settings) == 0 {
+    return false, "ERROR: settings validation failed -- no settings table provided in TOML."
+  } else if value, ok := s.Settings["kubeContext"]; !ok || value == "" {
+    return false, "ERROR: settings validation failed -- you have not provided a " +
+      "kubeContext to use. Can't work without it. Sorry!"
+  } else if value, ok = s.Settings["clusterURI"]; ok {
 
-		s.Settings["clusterURI"] = subsituteEnv(value)
-		if _, err := url.ParseRequestURI(s.Settings["clusterURI"]); err != nil {
-			return false, "ERROR: settings validation failed -- clusterURI must have a valid URL set in an env varibale or passed directly. Either the env var is missing/empty or the URL is invalid."
-		}
+    s.Settings["clusterURI"] = os.ExpandEnv(value)
+    if _, err := url.ParseRequestURI(s.Settings["clusterURI"]); err != nil {
+      return false, "ERROR: settings validation failed -- clusterURI must have a valid URL set in an env varibale or passed directly. Either the env var is missing/empty or the URL is invalid."
+    }
 
-		if _, ok = s.Settings["username"]; !ok {
-			return false, "ERROR: settings validation failed -- username must be provided if clusterURI is defined."
-		}
-		if value, ok = s.Settings["password"]; ok {
-			s.Settings["password"] = subsituteEnv(value)
-		} else {
-			return false, "ERROR: settings validation failed -- password must be provided if clusterURI is defined."
-		}
+    if _, ok = s.Settings["username"]; !ok {
+      return false, "ERROR: settings validation failed -- username must be provided if clusterURI is defined."
+    }
+    if value, ok = s.Settings["password"]; ok {
+      s.Settings["password"] = os.ExpandEnv(value)
+    } else {
+      return false, "ERROR: settings validation failed -- password must be provided if clusterURI is defined."
+    }
 
-		if s.Settings["password"] == "" {
-			return false, "ERROR: settings validation failed -- password should be set as an env variable. It is currently missing or empty. "
-		}
-	}
+    if s.Settings["password"] == "" {
+      return false, "ERROR: settings validation failed -- password should be set as an env variable. It is currently missing or empty. "
+    }
+  }
 
-	// certificates
-	if s.Certificates != nil && len(s.Certificates) != 0 {
-		_, ok1 := s.Settings["clusterURI"]
-		_, ok2 := s.Certificates["caCrt"]
-		_, ok3 := s.Certificates["caKey"]
-		if ok1 && (!ok2 || !ok3) {
-			return false, "ERROR: certifications validation failed -- You want me to connect to your cluster for you " +
-				"but have not given me the cert/key to do so. Please add [caCrt] and [caKey] under Certifications. You might also need to provide [clientCrt]."
-		} else if ok1 {
-			for key, value := range s.Certificates {
-				r, path := isValidCert(value)
-				if !r {
-					return false, "ERROR: certifications validation failed -- [ " + key + " ] must be a valid S3 or GCS bucket URL or a valid relative file path."
-				}
-				s.Certificates[key] = path
-			}
-		} else {
-			log.Println("INFO: certificates provided but not needed. Skipping certificates validation.")
-		}
+  // certificates
+  if s.Certificates != nil && len(s.Certificates) != 0 {
+    _, ok1 := s.Settings["clusterURI"]
+    _, ok2 := s.Certificates["caCrt"]
+    _, ok3 := s.Certificates["caKey"]
+    if ok1 && (!ok2 || !ok3) {
+      return false, "ERROR: certifications validation failed -- You want me to connect to your cluster for you " +
+        "but have not given me the cert/key to do so. Please add [caCrt] and [caKey] under Certifications. You might also need to provide [clientCrt]."
+    } else if ok1 {
+      for key, value := range s.Certificates {
+        r, path := isValidCert(value)
+        if !r {
+          return false, "ERROR: certifications validation failed -- [ " + key + " ] must be a valid S3 or GCS bucket URL or a valid relative file path."
+        }
+        s.Certificates[key] = path
+      }
+    } else {
+      log.Println("INFO: certificates provided but not needed. Skipping certificates validation.")
+    }
 
-	} else {
-		if _, ok := s.Settings["clusterURI"]; ok {
-			return false, "ERROR: certifications validation failed -- You want me to connect to your cluster for you " +
-				"but have not given me the cert/key to do so. Please add [caCrt] and [caKey] under Certifications. You might also need to provide [clientCrt]."
-		}
-	}
+  } else {
+    if _, ok := s.Settings["clusterURI"]; ok {
+      return false, "ERROR: certifications validation failed -- You want me to connect to your cluster for you " +
+        "but have not given me the cert/key to do so. Please add [caCrt] and [caKey] under Certifications. You might also need to provide [clientCrt]."
+    }
+  }
 
-	// namespaces
-	if nsOverride == "" {
-		if s.Namespaces == nil || len(s.Namespaces) == 0 {
-			return false, "ERROR: namespaces validation failed -- I need at least one namespace " +
-				"to work with!"
-		}
+  // namespaces
+  if nsOverride == "" {
+    if s.Namespaces == nil || len(s.Namespaces) == 0 {
+      return false, "ERROR: namespaces validation failed -- I need at least one namespace " +
+        "to work with!"
+    }
 
-		for k, v := range s.Namespaces {
-			if !v.InstallTiller && k != "kube-system" {
-				log.Println("INFO: namespace validation -- Tiller is not desired to be deployed in namespace [ " + k + " ].")
-			} else {
-				if tillerTLSEnabled(k) {
-					// validating the TLS certs and keys for Tiller
-					// if they are valid, their values (if they are env vars) are substituted
-					var ok1, ok2, ok3, ok4, ok5 bool
-					ok1, v.CaCert = isValidCert(v.CaCert)
-					ok2, v.ClientCert = isValidCert(v.ClientCert)
-					ok3, v.ClientKey = isValidCert(v.ClientKey)
-					ok4, v.TillerCert = isValidCert(v.TillerCert)
-					ok5, v.TillerKey = isValidCert(v.TillerKey)
-					if !ok1 || !ok2 || !ok3 || !ok4 || !ok5 {
-						return false, "ERROR: namespaces validation failed  -- some certs/keys are not valid for Tiller TLS in namespace [ " + k + " ]."
-					}
-					log.Println("INFO: namespace validation -- Tiller is desired to be deployed with TLS in namespace [ " + k + " ]. ")
-				} else {
-					log.Println("INFO: namespace validation -- Tiller is desired to be deployed WITHOUT TLS in namespace [ " + k + " ]. ")
-				}
-			}
-		}
-	} else {
-		log.Println("INFO: ns-override is used to override all namespaces with [ " + nsOverride + " ] Skipping defined namespaces validation.")
-	}
+    for k, v := range s.Namespaces {
+      if !v.InstallTiller && k != "kube-system" {
+        log.Println("INFO: naemspace validation -- Tiller is not desired to be deployed in namespace [ " + k + " ].")
+      } else {
+        if tillerTLSEnabled(k) {
+          // validating the TLS certs and keys for Tiller
+          // if they are valid, their values (if they are env vars) are substituted
+          var ok1, ok2, ok3, ok4, ok5 bool
+          ok1, v.CaCert = isValidCert(v.CaCert)
+          ok2, v.ClientCert = isValidCert(v.ClientCert)
+          ok3, v.ClientKey = isValidCert(v.ClientKey)
+          ok4, v.TillerCert = isValidCert(v.TillerCert)
+          ok5, v.TillerKey = isValidCert(v.TillerKey)
+          if !ok1 || !ok2 || !ok3 || !ok4 || !ok5 {
+            return false, "ERROR: namespaces validation failed  -- some certs/keys are not valid for Tiller TLS in namespace [ " + k + " ]."
+          }
+          log.Println("INFO: namespace validation -- Tiller is desired to be deployed with TLS in namespace [ " + k + " ]. ")
+        } else {
+          log.Println("INFO: namespace validation -- Tiller is desired to be deployed WITHOUT TLS in namespace [ " + k + " ]. ")
+        }
+      }
+    }
+  } else {
+    log.Println("INFO: ns-override is used to override all namespaces with [ " + nsOverride + " ] Skipping defined namespaces validation.")
+  }
 
-	// repos
-	if s.HelmRepos == nil || len(s.HelmRepos) == 0 {
-		return false, "ERROR: repos validation failed -- I need at least one helm repo " +
-			"to work with!"
-	}
-	for k, v := range s.HelmRepos {
-		_, err := url.ParseRequestURI(v)
-		if err != nil {
-			return false, "ERROR: repos validation failed -- repo [" + k + " ] " +
-				"must have a valid URL."
-		}
+  // repos
+  if s.HelmRepos == nil || len(s.HelmRepos) == 0 {
+    return false, "ERROR: repos validation failed -- I need at least one helm repo " +
+      "to work with!"
+  }
+  for k, v := range s.HelmRepos {
+    _, err := url.ParseRequestURI(v)
+    if err != nil {
+      return false, "ERROR: repos validation failed -- repo [" + k + " ] " +
+        "must have a valid URL."
+    }
 
-		continue
+    continue
 
-	}
+  }
 
-	// apps
-	if s.Apps == nil {
-		log.Println("INFO: You have not specified any apps. I have nothing to do. ",
-			"Horraayyy!.")
-		os.Exit(0)
-	}
+  // apps
+  if s.Apps == nil {
+    log.Println("INFO: You have not specified any apps. I have nothing to do. ",
+      "Horraayyy!.")
+    os.Exit(0)
+  }
 
-	names := make(map[string]bool)
-	for appLabel, r := range s.Apps {
-		result, errMsg := validateRelease(r, names, s)
-		if !result {
-			return false, "ERROR: apps validation failed -- for app [" + appLabel + " ]. " + errMsg
-		}
-	}
+  names := make(map[string]bool)
+  for appLabel, r := range s.Apps {
+    result, errMsg := validateRelease(r, names, s)
+    if !result {
+      return false, "ERROR: apps validation failed -- for app [" + appLabel + " ]. " + errMsg
+    }
+  }
 
-	return true, ""
-}
-
-// expand the environment variables if present. Useful for pipelines and gitlab runners.
-func subsituteEnv(name string) string {
-	return os.ExpandEnv(name)
+  return true, ""
 }
 
 // isValidCert checks if a certificate/key path/URI is valid
 func isValidCert(value string) (bool, string) {
-	tmp := subsituteEnv(value)
-	_, err1 := url.ParseRequestURI(tmp)
-	_, err2 := os.Stat(tmp)
-	if err2 != nil && (err1 != nil || (!strings.HasPrefix(tmp, "s3://") && !strings.HasPrefix(tmp, "gs://"))) {
-		return false, ""
-	}
-	return true, tmp
+  tmp := os.ExpandEnv(value)
+  _, err1 := url.ParseRequestURI(tmp)
+  _, err2 := os.Stat(tmp)
+  if err2 != nil && (err1 != nil || (!strings.HasPrefix(tmp, "s3://") && !strings.HasPrefix(tmp, "gs://"))) {
+    return false, ""
+  }
+  return true, tmp
 }
 
 // tillerTLSEnabled checks if Tiller is desired to be deployed with TLS enabled for a given namespace
 // TLS is considered desired ONLY if all certs and keys for both Tiller and the Helm client are defined.
 func tillerTLSEnabled(namespace string) bool {
 
-	ns := s.Namespaces[namespace]
-	if ns.CaCert != "" && ns.TillerCert != "" && ns.TillerKey != "" && ns.ClientCert != "" && ns.ClientKey != "" {
-		return true
-	}
-	return false
+  ns := s.Namespaces[namespace]
+  if ns.CaCert != "" && ns.TillerCert != "" && ns.TillerKey != "" && ns.ClientCert != "" && ns.ClientKey != "" {
+    return true
+  }
+  return false
 }
 
 // print prints the desired state
 func (s state) print() {
 
-	fmt.Println("\nMetadata: ")
-	fmt.Println("--------- ")
-	printMap(s.Metadata)
-	fmt.Println("\nCertificates: ")
-	fmt.Println("--------- ")
-	printMap(s.Certificates)
-	fmt.Println("\nSettings: ")
-	fmt.Println("--------- ")
-	printMap(s.Settings)
-	fmt.Println("\nNamespaces: ")
-	fmt.Println("------------- ")
-	printNamespacesMap(s.Namespaces)
-	fmt.Println("\nRepositories: ")
-	fmt.Println("------------- ")
-	printMap(s.HelmRepos)
-	fmt.Println("\nApplications: ")
-	fmt.Println("--------------- ")
-	for _, r := range s.Apps {
-		r.print()
-	}
+  fmt.Println("\nMetadata: ")
+  fmt.Println("--------- ")
+  printMap(s.Metadata)
+  fmt.Println("\nCertificates: ")
+  fmt.Println("--------- ")
+  printMap(s.Certificates)
+  fmt.Println("\nSettings: ")
+  fmt.Println("--------- ")
+  printMap(s.Settings)
+  fmt.Println("\nNamespaces: ")
+  fmt.Println("------------- ")
+  printNamespacesMap(s.Namespaces)
+  fmt.Println("\nRepositories: ")
+  fmt.Println("------------- ")
+  printMap(s.HelmRepos)
+  fmt.Println("\nApplications: ")
+  fmt.Println("--------------- ")
+  for _, r := range s.Apps {
+    r.print()
+  }
 }
+
--- utils.go
+++ utils.go
@@ -1,220 +1,208 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
+  "bytes"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "log"
+  "os"
+  "path/filepath"
+  "strconv"
+  "strings"
 
-	"github.com/BurntSushi/toml"
-	"github.com/Praqma/helmsman/aws"
-	"github.com/Praqma/helmsman/gcs"
+  "github.com/BurntSushi/toml"
+  "github.com/Praqma/helmsman/aws"
+  "github.com/Praqma/helmsman/gcs"
 )
 
 // printMap prints to the console any map of string keys and values.
 func printMap(m map[string]string) {
-	for key, value := range m {
-		fmt.Println(key, " : ", value)
-	}
+  for key, value := range m {
+    fmt.Println(key, " : ", value)
+  }
 }
 
 // printObjectMap prints to the console any map of string keys and object values.
 func printNamespacesMap(m map[string]namespace) {
-	for key, value := range m {
-		fmt.Println(key, " : protected = ", value)
-	}
+  for key, value := range m {
+    fmt.Println(key, " : protected = ", value)
+  }
 }
 
 // fromTOML reads a toml file and decodes it to a state type.
 // It uses the BurntSuchi TOML parser which throws an error if the TOML file is not valid.
 func fromTOML(file string, s *state) (bool, string) {
 
-	if _, err := toml.DecodeFile(file, s); err != nil {
-		return false, err.Error()
-	}
-	return true, "INFO: Parsed [[ " + file + " ]] successfully and found [ " + strconv.Itoa(len(s.Apps)) + " ] apps."
+  if _, err := toml.DecodeFile(file, s); err != nil {
+    return false, err.Error()
+  }
+  return true, "INFO: Parsed [[ " + file + " ]] successfully and found [ " + strconv.Itoa(len(s.Apps)) + " ] apps."
 
 }
 
 // toTOML encodes a state type into a TOML file.
 // It uses the BurntSuchi TOML parser.
 func toTOML(file string, s *state) {
-	log.Println("printing generated toml ... ")
-	var buff bytes.Buffer
-	var (
-		newFile *os.File
-		err     error
-	)
+  log.Println("printing generated toml ... ")
+  var buff bytes.Buffer
+  var (
+    newFile *os.File
+    err     error
+  )
 
-	if err := toml.NewEncoder(&buff).Encode(s); err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	newFile, err = os.Create(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bytesWritten, err := newFile.Write(buff.Bytes())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Wrote %d bytes.\n", bytesWritten)
-	newFile.Close()
+  if err := toml.NewEncoder(&buff).Encode(s); err != nil {
+    log.Fatal(err)
+    os.Exit(1)
+  }
+  newFile, err = os.Create(file)
+  if err != nil {
+    log.Fatal(err)
+  }
+  bytesWritten, err := newFile.Write(buff.Bytes())
+  if err != nil {
+    log.Fatal(err)
+  }
+  log.Printf("Wrote %d bytes.\n", bytesWritten)
+  newFile.Close()
 }
 
 // isOfType checks if the file extension of a filename/path is the same as "filetype".
 // isisOfType is case insensitive. filetype should contain the "." e.g. ".yaml"
 func isOfType(filename string, filetype string) bool {
-	return filepath.Ext(strings.ToLower(filename)) == strings.ToLower(filetype)
+  return filepath.Ext(strings.ToLower(filename)) == strings.ToLower(filetype)
 }
 
 // readFile returns the content of a file as a string.
 // takes a file path as input. It throws an error and breaks the program execution if it failes to read the file.
 func readFile(filepath string) string {
-	data, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		log.Fatal("ERROR: failed to read [ " + filepath + " ] file content: " + err.Error())
-	}
-	return string(data)
+  data, err := ioutil.ReadFile(filepath)
+  if err != nil {
+    log.Fatal("ERROR: failed to read [ " + filepath + " ] file content: " + err.Error())
+  }
+  return string(data)
 }
 
 // printHelp prints Helmsman commands
 func printHelp() {
-	fmt.Println("Helmsman version: " + version)
-	fmt.Println("Helmsman is a Helm Charts as Code tool which allows you to automate the deployment/management of your Helm charts.")
-	fmt.Println("Usage: helmsman [options]")
-	fmt.Println()
-	fmt.Println("Options:")
-	fmt.Println("--f                specifies the desired state TOML file.")
-	fmt.Println("--debug            prints basic logs during execution.")
-	fmt.Println("--apply            generates and applies an action plan.")
-	fmt.Println("--verbose          prints more verbose logs during execution.")
-	fmt.Println("--ns-override      override defined namespaces with a provided one.")
-	fmt.Println("--skip-validation  generates and applies an action plan.")
-	fmt.Println("--help             prints Helmsman help.")
-	fmt.Println("--v                prints Helmsman version.")
+  fmt.Println("Helmsman version: " + version)
+  fmt.Println("Helmsman is a Helm Charts as Code tool which allows you to automate the deployment/management of your Helm charts.")
+  fmt.Println("Usage: helmsman [options]")
+  fmt.Println()
+  fmt.Println("Options:")
+  fmt.Println("--f                specifies the desired state TOML file.")
+  fmt.Println("--debug            prints basic logs during execution.")
+  fmt.Println("--apply            generates and applies an action plan.")
+  fmt.Println("--verbose          prints more verbose logs during execution.")
+  fmt.Println("--ns-override      override defined namespaces with a provided one.")
+  fmt.Println("--skip-validation  generates and applies an action plan.")
+  fmt.Println("--help             prints Helmsman help.")
+  fmt.Println("--v                prints Helmsman version.")
 }
 
 // logVersions prints the versions of kubectl and helm to the logs
 func logVersions() {
-	cmd := command{
-		Cmd:         "bash",
-		Args:        []string{"-c", "kubectl version"},
-		Description: "Kubectl version: ",
-	}
+  cmd := command{
+    Cmd:         "bash",
+    Args:        []string{"-c", "kubectl version"},
+    Description: "Kubectl version: ",
+  }
 
-	exitCode, result := cmd.exec(debug, false)
-	if exitCode != 0 {
-		log.Fatal("ERROR: while checking kubectl version: " + result)
-	}
+  exitCode, result := cmd.exec(debug, false)
+  if exitCode != 0 {
+    log.Fatal("ERROR: while checking kubectl version: " + result)
+  }
 
-	log.Println("VERBOSE: kubectl version: \n " + result + "\n")
+  log.Println("VERBOSE: kubectl version: \n " + result + "\n")
 
-	cmd = command{
-		Cmd:         "bash",
-		Args:        []string{"-c", "helm version"},
-		Description: "Helm version: ",
-	}
+  cmd = command{
+    Cmd:         "bash",
+    Args:        []string{"-c", "helm version"},
+    Description: "Helm version: ",
+  }
 
-	exitCode, result = cmd.exec(debug, false)
-	if exitCode != 0 {
-		log.Fatal("ERROR: while checking helm version: " + result)
-	}
-	log.Println("VERBOSE: helm version: \n" + result + "\n")
-}
-
-// envVarExists checks if an environment variable is set or not and returns it.
-// empty string is returned for unset env vars
-// it accepts env var with/without '$' at the beginning
-func envVarExists(v string) (bool, string) {
-
-	if strings.HasPrefix(v, "$") {
-		v = strings.SplitAfter(v, "$")[1]
-	}
-
-	value, ok := os.LookupEnv(v)
-	return ok, value
+  exitCode, result = cmd.exec(debug, false)
+  if exitCode != 0 {
+    log.Fatal("ERROR: while checking helm version: " + result)
+  }
+  log.Println("VERBOSE: helm version: \n" + result + "\n")
 }
 
 // sliceContains checks if a string slice contains a given string
 func sliceContains(slice []string, s string) bool {
-	for _, a := range slice {
-		if strings.TrimSpace(a) == s {
-			return true
-		}
-	}
-	return false
+  for _, a := range slice {
+    if strings.TrimSpace(a) == s {
+      return true
+    }
+  }
+  return false
 }
 
 // validateServiceAccount checks if k8s service account exists in a given namespace
 func validateServiceAccount(sa string, namespace string) (bool, string) {
-	if namespace == "" {
-		namespace = "default"
-	}
-	ns := " -n " + namespace
+  if namespace == "" {
+    namespace = "default"
+  }
+  ns := " -n " + namespace
 
-	cmd := command{
-		Cmd:         "bash",
-		Args:        []string{"-c", "kubectl get serviceaccount " + sa + ns},
-		Description: "validating that serviceaccount [ " + sa + " ] exists in namespace [ " + namespace + " ].",
-	}
+  cmd := command{
+    Cmd:         "bash",
+    Args:        []string{"-c", "kubectl get serviceaccount " + sa + ns},
+    Description: "validating that serviceaccount [ " + sa + " ] exists in namespace [ " + namespace + " ].",
+  }
 
-	if exitCode, err := cmd.exec(debug, verbose); exitCode != 0 {
-		return false, err
-	}
-	return true, ""
+  if exitCode, err := cmd.exec(debug, verbose); exitCode != 0 {
+    return false, err
+  }
+  return true, ""
 }
 
 // downloadFile downloads a file from GCS or AWS buckets and name it with a given outfile
 // if downloaded, returns the outfile name. If the file path is local file system path, it is returned as is.
 func downloadFile(path string, outfile string) string {
-	if strings.HasPrefix(path, "s3") {
+  if strings.HasPrefix(path, "s3") {
 
-		tmp := getBucketElements(path)
-		aws.ReadFile(tmp["bucketName"], tmp["filePath"], outfile)
+    tmp := getBucketElements(path)
+    aws.ReadFile(tmp["bucketName"], tmp["filePath"], outfile)
 
-	} else if strings.HasPrefix(path, "gs") {
+  } else if strings.HasPrefix(path, "gs") {
 
-		tmp := getBucketElements(path)
-		gcs.ReadFile(tmp["bucketName"], tmp["filePath"], outfile)
+    tmp := getBucketElements(path)
+    gcs.ReadFile(tmp["bucketName"], tmp["filePath"], outfile)
 
-	} else {
+  } else {
 
-		log.Println("INFO: " + outfile + " will be used from local file system.")
-		copyFile(path, outfile)
-	}
-	return outfile
+    log.Println("INFO: " + outfile + " will be used from local file system.")
+    copyFile(path, outfile)
+  }
+  return outfile
 }
 
 // copyFile copies a file from source to destination
 func copyFile(source string, destination string) {
-	from, err := os.Open(source)
-	if err != nil {
-		log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
-	}
-	defer from.Close()
+  from, err := os.Open(source)
+  if err != nil {
+    log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
+  }
+  defer from.Close()
 
-	to, err := os.OpenFile(destination, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
-	}
-	defer to.Close()
+  to, err := os.OpenFile(destination, os.O_RDWR|os.O_CREATE, 0666)
+  if err != nil {
+    log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
+  }
+  defer to.Close()
 
-	_, err = io.Copy(to, from)
-	if err != nil {
-		log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
-	}
+  _, err = io.Copy(to, from)
+  if err != nil {
+    log.Fatal("ERROR: while copying " + source + " to " + destination + " : " + err.Error())
+  }
 }
 
 // deleteFile deletes a file
 func deleteFile(path string) {
-	log.Println("INFO: cleaning up ... deleting " + path)
-	if err := os.Remove(path); err != nil {
-		log.Fatal("ERROR: could not delete file: " + path)
-	}
+  log.Println("INFO: cleaning up ... deleting " + path)
+  if err := os.Remove(path); err != nil {
+    log.Fatal("ERROR: could not delete file: " + path)
+  }
 }
+
