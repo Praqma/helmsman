e0666377ac23f48084675d21f2a96f42cf6f6948
sami-alajrami <sami@praqma.com>
2018-08-07 14:00:35 +0200

    fixing tests

--- plan_test.go
+++ plan_test.go
@@ -62,7 +62,8 @@ func Test_plan_addCommand(t *testing.T) {
 				Decisions: tt.fields.Decisions,
 				Created:   tt.fields.Created,
 			}
-			p.addCommand(tt.args.c, 0)
+			r := &release{}
+			p.addCommand(tt.args.c, 0, r)
 			if got := len(p.Commands); got != 1 {
 				t.Errorf("addCommand(): got  %v, want 1", got)
 			}
