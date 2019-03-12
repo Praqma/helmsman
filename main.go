75c409a20afbb18ad9572012b928fb6b47a0307e
sami-alajrami <sami@praqma.com>
2019-03-12 22:30:03 +0100

    releasing v1.8.0

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var shouldCleanup bool
 var skipValidation bool
 var applyLabels bool
 var keepUntrackedReleases bool
-var appVersion = "v1.7.4"
+var appVersion = "v1.8.0"
 var helmVersion string
 var kubectlVersion string
 var dryRun bool
