59fc4eabf85b2ba54791b5e77f2f450b4fb0c1bb
sami-alajrami <sami@praqma.com>
2019-06-26 15:12:18 +0200

    releasing v1.10.0

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var nsOverride string
 var skipValidation bool
 var applyLabels bool
 var keepUntrackedReleases bool
-var appVersion = "v1.9.1"
+var appVersion = "v1.10.0"
 var helmVersion string
 var kubectlVersion string
 var dryRun bool
