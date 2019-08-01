0f5e5f68672e19c095b925949161e2cde3000a24
sami-alajrami <sami@praqma.com>
2019-08-01 14:14:49 +0200

    bumping version

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var nsOverride string
 var skipValidation bool
 var applyLabels bool
 var keepUntrackedReleases bool
-var appVersion = "v1.10.1"
+var appVersion = "v1.11.0"
 var helmVersion string
 var kubectlVersion string
 var dryRun bool
