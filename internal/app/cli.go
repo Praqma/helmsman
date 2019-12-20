614c633b836378c88ab82da1fddea1c0ff91f555
sami-alajrami <sami@praqma.com>
2019-12-20 13:28:38 +0100

    add a check for the allowed helm versions

--- internal/app/cli.go
+++ internal/app/cli.go
@@ -3,10 +3,12 @@ package app
 import (
 	"flag"
 	"fmt"
-	"github.com/imdario/mergo"
-	"github.com/joho/godotenv"
 	"os"
 	"strings"
+
+	version "github.com/hashicorp/go-version"
+	"github.com/imdario/mergo"
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -94,7 +96,16 @@ func Cli() {
 	}
 
 	helmVersion = strings.TrimSpace(getHelmVersion())
-	log.Verbose("Helm client version: " + helmVersion)
+	extractedHelmVersion := helmVersion
+	if !strings.HasPrefix(helmVersion, "v") {
+		extractedHelmVersion = strings.TrimSpace(strings.Split(helmVersion, ":")[1])
+	}
+	log.Verbose("Helm client version: " + extractedHelmVersion)
+	v1, _ := version.NewVersion(extractedHelmVersion)
+	jsonConstraint, _ := version.NewConstraint(">=3.0.0")
+	if !jsonConstraint.Check(v1) {
+		log.Fatal("this version of Helmsman does not work with helm releases older than 3.0.0")
+	}
 
 	kubectlVersion = strings.TrimSpace(strings.SplitN(getKubectlClientVersion(), ": ", 2)[1])
 	log.Verbose("kubectl client version: " + kubectlVersion)
