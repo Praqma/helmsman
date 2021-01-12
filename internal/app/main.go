88438d7ae2f96ea1ca69134b5d8a153c20cd0d1e
Luis Davim <luis.davim@jet.com>
2021-01-12 17:47:03 +0000

    Prepeare release v3.6.3

--- internal/app/main.go
+++ internal/app/main.go
@@ -8,7 +8,7 @@ import (
 const (
 	helmBin            = "helm"
 	kubectlBin         = "kubectl"
-	appVersion         = "v3.6.2"
+	appVersion         = "v3.6.3"
 	tempFilesDir       = ".helmsman-tmp"
 	defaultContextName = "default"
 	resourcePool       = 10
