b30358ab94dde52ee317d4274e77826c770eacdf
Luis Davim <luis.davim@jet.com>
2021-03-13 13:31:38 +0000

    prepare release v3.6.6

--- internal/app/main.go
+++ internal/app/main.go
@@ -8,7 +8,7 @@ import (
 const (
 	helmBin            = "helm"
 	kubectlBin         = "kubectl"
-	appVersion         = "v3.6.5"
+	appVersion         = "v3.6.6"
 	tempFilesDir       = ".helmsman-tmp"
 	defaultContextName = "default"
 	resourcePool       = 10
