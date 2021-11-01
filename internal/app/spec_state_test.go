258c1649623c84d4f70b44d0ea570420e3b6eddd
adamarnold-msm <adam.arnold@moneysupermarket.com>
2021-11-01 10:45:53 +0000

    Add -spec flag to support specification file consisting of multiple desired state files state files, add priority override

---
+++ internal/app/spec_state_test.go
@@ -0,0 +1,50 @@
+package app
+
+import (
+	"testing"
+)
+
+func Test_specFromYAML(t *testing.T) {
+	type args struct {
+		file string
+		s    *StateFiles
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test case 1 -- Valid YAML",
+			args: args{
+				file: "../../examples/example-spec.yaml",
+				s:    new(StateFiles),
+			},
+			want: true,
+		}, {
+			name: "test case 2 -- Invalid Yaml",
+			args: args{
+				file: "../../tests/Invalid_example_spec.yaml",
+				s:    new(StateFiles),
+			},
+			want: false,
+		},
+	}
+
+	teardownTestCase := setupStateFileTestCase(t)
+	defer teardownTestCase(t)
+	for _, tt := range tests {
+		// os.Args = append(os.Args, "-f ../../examples/example.yaml")
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.args.s.specFromYAML(tt.args.file)
+			if err != nil {
+				t.Log(err)
+			}
+
+			got := err == nil
+			if got != tt.want {
+				t.Errorf("specFromYaml() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
