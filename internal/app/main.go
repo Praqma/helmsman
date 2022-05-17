d3bde9bbbc2bbf16bed6bfc37e1ae1764df3da04
Luis Davim <luis.davim@sendoso.com>
2022-05-17 22:56:37 +0100

    release: v3.9.0

--- internal/app/main.go
+++ internal/app/main.go
@@ -8,7 +8,7 @@ import (
 const (
 	helmBin            = "helm"
 	kubectlBin         = "kubectl"
-	appVersion         = "v3.9.0"
+	appVersion         = "v3.9.1"
 	tempFilesDir       = ".helmsman-tmp"
 	defaultContextName = "default"
 	resourcePool       = 10
