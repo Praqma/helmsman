bd247448b1178c0f5bd02d4d903eda3ba26b1078
Mateusz Kubaczyk <12384536+mkubaczyk@users.noreply.github.com>
2022-07-25 08:32:29 +0200

    Release v3.13.0

--- internal/app/main.go
+++ internal/app/main.go
@@ -8,7 +8,7 @@ import (
 const (
 	helmBin            = "helm"
 	kubectlBin         = "kubectl"
-	appVersion         = "v3.12.0"
+	appVersion         = "v3.13.0"
 	tempFilesDir       = ".helmsman-tmp"
 	defaultContextName = "default"
 	resourcePool       = 10
